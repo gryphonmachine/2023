206d1e79f42632de3987786e75c263a20850986b
heybereket <semagnbereket@gmail.com>
2023-03-18 03:54:33 -0400

    autonomous chooser

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.cameraserver.CameraServer;
@@ -11,15 +7,12 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-/**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
- * project.
- */
+import frc.robot.commands.ChargeStationMovements;
+
 public class Robot extends TimedRobot {
 
   private Command m_autonomousCommand;
@@ -27,15 +20,15 @@ public class Robot extends TimedRobot {
   private RobotContainer m_robotContainer;
   public static NetworkTable limelight;
   public static NetworkTableEntry ll_x;
-  public static NetworkTableEntry ll_y;/**
-   * This function is run when the robot is first started up and should be used for any
-   * initialization code.
-   */
+  public static NetworkTableEntry ll_y;
 
+  private static final String kChargeStationAuto = "Charge Station";
+  private String m_autoSelected;
+  private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  // Runs when the robot is first started up
   @Override
   public void robotInit() {
-    // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
-    // autonomous chooser on the dashboard.
     RobotMap.init();
     m_robotContainer = new RobotContainer();
     limelight = NetworkTableInstance.getDefault().getTable("limelight");
@@ -44,26 +37,17 @@ public class Robot extends TimedRobot {
     CameraServer.startAutomaticCapture();
 
     SmartDashboard.putData("Calibrate Robot", RobotMap.calibrateGyro);
+    SmartDashboard.putData(m_chooser);
+
+    m_chooser.setDefaultOption("Charge Station Auto", kChargeStationAuto);
+    SmartDashboard.putData("Auto choices", m_chooser);
   }
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for items like
-   * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
-   *
-   * <p>This runs after the mode specific periodic functions, but before LiveWindow and
-   * SmartDashboard integrated updating.
-   */
   @Override
   public void robotPeriodic() {
-    // Runs the Scheduler.  This is responsible for polling buttons, adding newly-scheduled
-    // commands, running already-scheduled commands, removing finished or interrupted commands,
-    // and running subsystem periodic() methods.  This must be called from the robot's periodic
-    // block in order for anything in the Command-based framework to work.
-
     CommandScheduler.getInstance().run();
   }
 
-  /** This function is called once each time the robot enters Disabled mode. */
   @Override
   public void disabledInit() {
     System.out.println(RobotMap.getLimelightArea());
@@ -74,10 +58,12 @@ public class Robot extends TimedRobot {
     System.out.println(RobotMap.getLimelightArea());
   }
 
-  /** This autonomous runs the autonomous command selected by your {@link RobotContainer} class. */
   @Override
   public void autonomousInit() {
     m_autonomousCommand = m_robotContainer.getAutonomousCommand();
+
+    m_autoSelected = m_chooser.getSelected();
+    System.out.println("Auto Mode selected: " + m_autoSelected);
 
     // schedule the autonomous command (example)
     if (m_autonomousCommand != null) {
@@ -85,16 +71,17 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called periodically during autonomous. */
   @Override
-  public void autonomousPeriodic() {}
+  public void autonomousPeriodic() {
+    switch (m_autoSelected) {
+      case kChargeStationAuto:
+        new ChargeStationMovements();
+        break;
+    }
+  }
 
   @Override
   public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
     if (m_autonomousCommand != null) {
       m_autonomousCommand.cancel();
     }
@@ -106,7 +93,6 @@ public class Robot extends TimedRobot {
     DriverStation.reportWarning("Finished Calibrating IMU.", true);
   }
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
     if (isEnabled()) {
@@ -119,11 +105,9 @@ public class Robot extends TimedRobot {
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {}
 }
