613efa1698e952c75bf056612800651b46a0bfee
heybereket <semagnbereket@gmail.com>
2023-03-22 19:57:02 -0400

    ok

--- README.md
+++ README.md
@@ -1,6 +1,25 @@
 # FIRST Robotics: FRC 6070
 2023 Codebase
 
+## Autonomous Routine
+```bash
+Move robot forwards for X seconds to pass community zone at X speed,
+then turns 180 degrees getting ready for teleop
+
+Points Gained: 3
+   
+---------------------------
+
+Score a cube or cone on top row
+Note: Game pieces come pre-loaded onto the arm, nothing has to be picked up
+   
+Points Gained: 6
+
+---------------------------
+
+Total Autonomous Points: 9
+```
+
 ### Authors
 - Bereket Semagn
 - Roozbeh Ali
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -5,16 +5,15 @@ import edu.wpi.first.cscore.UsbCamera;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
-import frc.robot.subsystems.Drivetrain;
 
 public class Robot extends TimedRobot {
 
   // Autonomous: Motor Speeds
-  private static final double FORWARD_SPEED = 0.2; // Speed to move forward at
-  private static final double BACKWARD_SPEED = -0.2; // Speed to move backward at
-  private static final double FORWARD_TIME = 3.0; // Time to move forward in seconds
+  private static final double FORWARD_SPEED = 0.2;
+  private static final double FORWARD_TIME = 5.0; // seconds
 
-  private static final double BACKWARD_TIME = 5.0; // Time to move backward in seconds
+  // private static final double BACKWARD_SPEED = -0.2;
+  // private static final double BACKWARD_TIME = 5.0; // seconds
 
   @Override
   public void robotInit() {
@@ -39,22 +38,6 @@ public class Robot extends TimedRobot {
   @Override
   public void disabledPeriodic() {}
 
-  /**
-   * Autonomous Routine:
-   * Move robot forwards for X seconds to pass community zone at X speed,
-   * then turns 180 degrees getting ready for teleop
-   *
-   * Points Gained: 3
-   *
-   * ---------------------------
-   *
-   * Score a cube or cone on top row
-   * Note: Game pieces come pre-loaded onto the arm, nothing has to be picked up
-   *
-   * Points Gained: 6
-   *
-   * Total Autonomous Points: 9
-   */
   @Override
   public void autonomousInit() {
     Timer timer = new Timer();
